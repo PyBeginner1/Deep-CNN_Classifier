dcdd896e43e56a0c9848caa5056ff3ea6abd5223
NightNight1 <69844203+NightNight1@users.noreply.github.com>
2023-02-12 11:04:44 +0530

    log updated

--- src/deepClassifier/__init__.py
+++ src/deepClassifier/__init__.py
@@ -1,20 +1,18 @@
 import os
 import sys
-import logging 
+import logging
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
 log_dir = "logs"
-log_filepath = os.path.join(log_dir,'running_logs.log')
-os.makedirs(log_dir, exist_ok=True) 
+log_filepath = os.path.join(log_dir, "running_logs.log")
+os.makedirs(log_dir, exist_ok=True)
 
 logging.basicConfig(
-    level = logging.INFO,
-    format = logging_str, 
-    handlers = [
-        logging.FileHandler(log_filepath),          #Logs into the mentioned file   
-        logging.StreamHandler(sys.stdout)           #Shows logs in the terminal
-    ]
-)
-
+    level=logging.INFO, 
+    format=logging_str,
+    handlers=[
+        logging.FileHandler(log_filepath),              #Logs into the mentioned file   
+        logging.StreamHandler(sys.stdout),              #Shows logs in the terminal
+    ])
 
 logger = logging.getLogger("deepClassifierLogger")      #getLogger() function accepts a single argument - the logger's name.
