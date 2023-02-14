cbf76730d52e337633362de7f69a52a5d4cd52ff
NightNight1 <69844203+NightNight1@users.noreply.github.com>
2023-02-14 09:04:51 +0530

    test cases

--- src/deepClassifier/entity/config_entity.py
+++ src/deepClassifier/entity/config_entity.py
@@ -1,10 +1,10 @@
 from dataclasses import dataclass
-from pathlib import Path 
+from pathlib import Path
 
 
 @dataclass(frozen=True)
 class DataIngestionConfig:
-    root_dir: Path 
-    source_URL: str 
+    root_dir: Path
+    source_URL: str
     local_data_file: Path
     unzip_dir: Path
