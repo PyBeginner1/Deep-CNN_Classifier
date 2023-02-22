36680bcadc3d71d5c011f4d13019a7ae84d61f49
NightNight1 <69844203+NightNight1@users.noreply.github.com>
2023-02-22 14:49:37 +0530

    Configuration for Evaluation completed

--- src/deepClassifier/config/configuration.py
+++ src/deepClassifier/config/configuration.py
@@ -1,9 +1,9 @@
 from deepClassifier.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
 from deepClassifier.utils.common import *
 from deepClassifier.entity import (DataIngestionConfig, PrepareBaseModelConfig, 
-                                   PrepareCallbacksConfig, TrainingConfig)
+                                   PrepareCallbacksConfig, TrainingConfig, EvaluationConfig)
 import os
-
+from deepClassifier import logger
 
 class ConfigurationManager:
     def __init__(self, 
@@ -35,7 +35,7 @@ class ConfigurationManager:
                 local_data_file= local_data_file,
                 unzip_dir= unzip_dir
             )
-
+            logger.info(f"Data Ingestion config: {data_ingestion_config}")
             return data_ingestion_config
         except Exception as e:
             raise e
@@ -68,6 +68,7 @@ class ConfigurationManager:
                 params_classes=params_classes
             )
             
+            logger.info(f"Prepare Base Model config: {prepare_base_model_config}")
             return prepare_base_model_config
         except Exception as e:
             raise e
@@ -90,6 +91,7 @@ class ConfigurationManager:
                 checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
             )
             
+            logger.info(f"Prepare Callback config: {prepare_callback_config}")
             return prepare_callback_config
         except Exception as e:
             raise e
@@ -114,6 +116,21 @@ class ConfigurationManager:
                 params_image_size=params.IMAGE_SIZE,
                 params_is_augmentation=params.AUGMENTATION
             )
+            logger.info(f"Training config: {training_config}")
             return training_config
+        except Exception as e:
+            raise e
+
+
+    def get_evaluation_config(self) -> EvaluationConfig:
+        try:
+            eval_config = EvaluationConfig(
+                path_of_model=self.config.training.trained_model_path,
+                training_data= self.config.data_ingestion.unzip_dir,
+                params_image_size=self.params.IMAGE_SIZE,
+                params_batch_size = self.params.BATCH_SIZE
+            )
+            logger.info(f"Evaluation config: {eval_config}")
+            return eval_config
         except Exception as e:
             raise e
